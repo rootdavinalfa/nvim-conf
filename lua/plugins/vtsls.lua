db404fb1398a728119d4561c6bd4d23f9c4fbc80
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2026-02-24 08:30:30 +0700

    [FEAT] Change vtsls maxMemory to 6GB

---
+++ lua/plugins/vtsls.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "AstroNvim/astrolsp",
+    ---@param opts AstroLSPOpts
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      table.insert(opts.servers, "vtsls")
+
+      opts.config = require("astrocore").extend_tbl(opts.config or {}, {
+        vtsls = {
+          settings = {
+            vtsls = {
+              tsserver = {
+                -- 6 GB in MB
+                maxTsServerMemory = 6144,
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "vtsls" })
+    end,
+  },
+}
